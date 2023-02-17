b4abf2018758a7463ecb908bde69cfdf2a44f1da
Jay226-cyber <jamesoshen77@gmail.com>
2023-02-17 15:56:26 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main- Entry point
- * @void: Null value
+ * main - A program that prints all numbers 00-99
  *
- * Description: Print numbers from 01 to 89
- * Return: Zero value
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
-	for (n = 01; n <= 89; n++)
+	for (n = 0; n <= 89; n++)
 	{
 		putchar((n / 10) + '0');
 		putchar((n % 10) + '0');
-		if (n != 89)
+		if (num != 89)
 		{
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
 		}
-	}
+		}
 	putchar('\n');
 	return (0);
 }
